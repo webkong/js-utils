9701cd7ae6db4dc2e1fa2408720410de6f1a3697
Shiwei Wang <772930508@qq.com>
2019-04-09 19:46:22 +0800

    fix

--- src/tools/index.js
+++ src/tools/index.js
@@ -5,9 +5,11 @@
  * @param {String=} url
  * @return {String}
  */
-exports.getUrlParam = function(name, url) {
+function getUrlParam(name, url) {
   var u = arguments[1] || window.location.search,
     reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
     r = u.substr(u.indexOf("?") + 1).match(reg);
   return r != null ? r[2] : "";
-};
+}
+
+exports.tools = { getUrlParam };
